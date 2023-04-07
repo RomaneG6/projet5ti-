75bb32e686f4103b6ba5f688c14d76abd8b8b378
Grootaers Romane <210469@CCM-Asty-Moulin.local>
2023-04-07 12:50:43 +0200

    accueil

--- index.php
+++ index.php
@@ -9,18 +9,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/base.css">
-    <title>StreaMovie</title>
+    <title>Metsar</title>
 </head>
 <body>
     <header>
         <ul class="flex space-evenly">
             <li class="menu"><a href="/">Home</a></li>
-            <li  class="menu"><a href="/profil">Page profil</a></li>
+            <li class="menu"><a href="/">Films</a></li>
+            <li class="menu"><a href="/">Séries</a></li>
+            <li class="menu"><a href="/">Anime</a></li>
+            <input id="searchbar" onkeyup="search_stream()" type="text"
+        name="search" placeholder="Rechercher...">
             <li  class="menu"> <?php if(isset($_SESSION['user'])): ?>
                                         <a href="deconnexion">Déconnexion</a>
                                         <?php else : ?>
                                         <a href="connexion">Connexion</a></li>
                                         <?php endif ?>
+            <li  class="menu"><a href="/profil">Page profil</a></li>                            
         </ul>
     </header>
     <main>
